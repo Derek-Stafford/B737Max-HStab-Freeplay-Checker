895bfc1c540624ffe889028bd1d410f606390c83
Boulder <157924017+Derek-Stafford@users.noreply.github.com>
2024-12-30 22:54:33 -0700

    Update Max_Calculator-1.py
    Changes:  1) "Cyan" cosmetic for value_check_1 to differ when values are set to 0, 2) Pass & Fail variables to track which values pass or fail, 3) Added results list to contain pass/fail results, 4) changed def negative_check function from if/else statement setup within to max() to reduce if/else statements, 5) updated value_check functions to include the pass/fail variables and Results list, 6) Added if/elif/else statement combo at the end to print either “all values passed” or “all values passed” and to loop through the results list for all other scenarios

--- Max_Calculator-1.py
+++ Max_Calculator-1.py
@@ -1,5 +1,6 @@
 # Cosmetic
 bold = '\033[1m'
+cyan = '\33[36m'
 end = '\033[0m'
 green = '\033[92m'
 italic = '\033[3m'
@@ -13,30 +14,32 @@ Weight = float(input(italic + bold + "Weight = "))
 
 # Condition checking if X, Y, or Z are negative
 def negative_check(value): 
-		if value < 0:
-				return 0
-		else:
-				return value
+  return max(0,value)
+
+Fail = 0 
+Pass = 0
+Results = []
 
 # Output Values and Threshold Messages
 def value_check_1(name, value, threshold):
-		if value == 0:
-				print(italic+bold+green+f"{name} = 0" + end)
-				print(italic+bold+green + f"Recheck {name}, otherwise {name} passed"+ end)
-		elif value > threshold:
-				print(bold+italic+red +f"{name} = {value:.4f}"+end)
-				print(bold+italic+red+f"{name} failed"+end)
-		else:
-				print(bold+italic+green + f"{name} = {value:.4f}"+end)
-				print(bold+italic+green + f"{name} Passed"+end)
+  global Fail, Pass
+  if value == 0:
+   Results.append(bold + italic + cyan + f"Recheck {name}: if correct, {name} = 0 and passed" + end)
+  elif value > threshold:
+   Fail += 1
+   Results.append(bold + italic + red + f"Failed: {name} = {value}, threshold = {threshold}" + end)
+  else:
+   Pass += 1
+  Results.append(bold + italic + green + f"Passed: {name} = {value}, threshold = {threshold}" + end)
 
 def value_check_2(name, value, threshold):
-		if value > threshold:
-				print(bold+italic+red+f"{name} = {value:.4f}"+end)
-				print(bold+italic+red + f"{name} failed" + end)
-		else:
-				print(bold+italic+green+f"{name} = {value:.4f}"+end)
-				print(bold+italic+green+f"{name} passed"+end)
+  global Fail, Pass
+  if value > threshold:
+    Fail += 1
+    Results.append(bold + italic + red + f"Failed: {name} = {value}, threshold = {threshold}" + end)
+  else:
+    Pass += 1
+    Results.append(bold + italic + green + f"Passed: {name} = {value}, threshold = {threshold}" + end)
 
 # Calculations
 X = negative_check(D1 - (Weight * 0.0000155))
@@ -49,3 +52,12 @@ value_check_1("X", X, 0.060)
 value_check_1("Y", Y, 0.060)
 value_check_1("Z", Z, 0.050)
 value_check_2("H", H, 0.051)
+
+# Summary of Results
+if Pass == 4: 
+  print(bold+italic+green+"All values passed"+end)
+elif Fail == 4:
+  print(bold+italic+red +"All values failed"+end)
+else: 
+  for result in Results:
+    print(result)
