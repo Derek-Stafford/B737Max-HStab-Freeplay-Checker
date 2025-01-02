41e4998f9d1602a435359b8f69f23e93914b20d9
Boulder <157924017+Derek-Stafford@users.noreply.github.com>
2025-01-02 03:53:04 -0700

    Update Max_Calculator-1.py

--- Max_Calculator-1.py
+++ Max_Calculator-1.py
@@ -6,7 +6,7 @@ green = '\033[92m'
 italic = '\033[3m'
 red = '\033[31m'
 
-# User Input Values
+# Input Values
 D1 = float(input(italic + bold + "D1 = "))
 D2 = float(input(italic + bold + "D2 = "))
 D3 = float(input(italic + bold + "D3 = "))
@@ -14,38 +14,39 @@ Weight = float(input(italic + bold + "Weight = "))
 
 # Condition checking if X, Y, or Z are negative
 def negative_check(value): 
-  return max(0,value)
+ return max(0,value)
 
 Fail = 0 
 Pass = 0
 Results = []
 
-# Output Values and Threshold Messages
+# Output Values and Threshold Messages for X, Y, and Z
 def value_check_1(name, value, threshold):
-  global Fail, Pass
+  global Pass, Fail
   if value == 0:
-   Results.append(bold + italic + cyan + f"Recheck {name}: if correct, {name} = 0 and passed" + end)
+   Results.append(bold+italic+cyan+f"Recheck {name}:\nif correct, {name} = 0 and passed"+end)
   elif value > threshold:
-   Fail += 1
-   Results.append(bold + italic + red + f"Failed: {name} = {value}, threshold = {threshold}" + end)
+    Fail += 1
+    Results.append(bold+italic+red+f"{name} Failed:\n{name} = {value:.4f}\nThreshold = {threshold:.3f}"+end)
   else:
-   Pass += 1
-  Results.append(bold + italic + green + f"Passed: {name} = {value}, threshold = {threshold}" + end)
+    Pass += 1
+    Results.append(bold+italic+green+f"{name} Passed:\n{name} = {value:.4f} \nThreshold = {threshold:.3f}"+end) 
 
+# Output Values and Threshold Messages for H only
 def value_check_2(name, value, threshold):
   global Fail, Pass
   if value > threshold:
     Fail += 1
-    Results.append(bold + italic + red + f"Failed: {name} = {value}, threshold = {threshold}" + end)
+    Results.append(bold+italic+red+f"{name} Failed:\n{name} = {value:.4f}\nThreshold = {threshold:.3f}"+end)
   else:
     Pass += 1
-    Results.append(bold + italic + green + f"Passed: {name} = {value}, threshold = {threshold}" + end)
+    Results.append(bold+italic+green+f"{name} Passed:\n{name} = {value:.4f}\nThreshold = {threshold:.3f}"+end) 
 
-# Calculations
-X = negative_check(D1 - (Weight * 0.0000155))
-Y = negative_check(D2 - (Weight * 0.0000155))
-Z = negative_check((D3 * 1.25) - (Weight * 0.0000318))
-H = Z + ((X + Y) / 2)
+# Overall Calculations
+X = negative_check(D1-(Weight*0.0000155))
+Y = negative_check(D2-(Weight *0.0000155))
+Z = negative_check((D3*1.25)-(Weight*0.0000318))
+H = Z+((X+Y)/2)
 
 # Threshold Checks
 value_check_1("X", X, 0.060)
@@ -55,9 +56,9 @@ value_check_2("H", H, 0.051)
 
 # Summary of Results
 if Pass == 4: 
-  print(bold+italic+green+"All values passed"+end)
+ print(bold+italic+green+"All values passed"+end)
 elif Fail == 4:
-  print(bold+italic+red +"All values failed"+end)
+  print(bold+italic+red+"All values failed"+end)
 else: 
   for result in Results:
     print(result)
